028747fab5fe70fe10e80ffa9b4bab07c5605c71
Sunzid <sunzidhassan@gmail.com>
2023-10-30 14:53:51 -0500

    Added sound in level 1 finish form.

--- Project/Fall2020_CSC403_Project/FrmLevel1Finish.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel1Finish.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Fall2020_CSC403_Project.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,12 @@ namespace Fall2020_CSC403_Project
 {
     public partial class FrmLevel1Finish : Form
     {
+        System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer();
         public FrmLevel1Finish()
         {
             InitializeComponent();
+            soundPlayer.SoundLocation = "C:\\Users\\sunzi\\Downloads\\Scrum Project\\Fall2020_CSC403_Project_BarbieHeimer\\Project\\Fall2020_CSC403_Project\\data\\audio\\Level1FinishClap.wav";
+            soundPlayer.Play();
         }
 
         private void BtnLevel1FinishMainMenu_Click(object sender, EventArgs e)
