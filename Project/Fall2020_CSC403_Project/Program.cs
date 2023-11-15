4c0fbb91f3f0d7507780534d7a5155ed42a0e5b1
Sunzid <sunzidhassan@gmail.com>
2023-11-15 03:40:13 -0600

    Bug fix

--- Project/Fall2020_CSC403_Project/Program.cs
+++ Project/Fall2020_CSC403_Project/Program.cs
@@ -13,7 +13,7 @@ namespace Fall2020_CSC403_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMainMenu());
+            Application.Run(new FrmIntro());
             //Application.Run(new FrmStatsSelection());
             //Application.Run(new FrmLevel2());
 
